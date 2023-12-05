68365cb26e597b366c37e548e89380b52ba8ad40
Ali bin shoaib <Ali.h.bin.shoaib@gmail.com>
2023-12-05 22:15:07 +0300

    implement checkWin function

--- Game.php
+++ Game.php
@@ -34,11 +34,15 @@ class Game
     }
     public function move(GameMove $gameMove): void
     {
+        if ($this->state !== GameStatue::IN_PLAY)
+            throw new Exception('Game is finished.');
         $this->checkIsValidPlayer($gameMove->player);
-        $this->checkIsValidMove($gameMove);
+        $this->checkIsValidMove($gameMove->x, $gameMove->y);
         $this->makeAMove($gameMove);
-        $this->checkWin();
+        $this->checkWin($gameMove->x, $gameMove->y);
         $this->showBoard();
+        if ($this->state === GameStatue::FINISHED)
+            echo "player {$this->lastPlayer->username} wins the game";
     }
     private function isValidPlayer(Player $player)
     {
@@ -50,10 +54,10 @@ class Game
             $this->player1 = $player;
         }
     }
-    private function checkIsValidMove(GameMove $gameMove)
+    private function checkIsValidMove(int $x, int $y)
     {
-        if ($gameMove->x >= self::N || $gameMove->y >= self::N || !empty($this->board[$gameMove->x][$gameMove->y]))
-            throw new Exception("Invalid Move! " . var_dump(json_encode(["x" => $gameMove->x, "y" => $gameMove->y, "player" => $gameMove->player->username])));
+        if ($x >= self::N || $y >= self::N || !empty($this->board[$x][$y]))
+            throw new Exception("Invalid Move! " . var_dump(json_encode(["x" => $x, "y" => $y])));
     }
     private function checkIsValidPlayer(Player $player)
     {
@@ -71,17 +75,51 @@ class Game
         $this->lastPlayer = $gameMove->player;
         $this->movesCount++;
     }
-    private function checkWin()
+    private function checkWin(int $x, int $y)
     {
+        $symbol = $this->lastPlayer === $this->player1 ? 'X' : 'O';
+        $isWin = true;
+        if ($this->movesCount >= self::N + 2) {
+            for ($i = $x; $i < self::N; $i++) {
+                for ($j = 0; $j < self::N; $j++) {
+                    if ($this->board[$x][$j] != $symbol) {
+                        $isWin = false;
+                        break;
+                    }
+                }
+                if ($isWin) {
+                    $this->state = GameStatue::FINISHED;
+                    break;
+                }
+                $isWin = true;
+                for ($j = 0; $j < self::N; $j++) {
+                    if ($this->board[$j][$i] != $symbol) {
+                        $isWin = false;
+                        break;
+                    }
+                }
+            }
+            if (!$isWin) {
+                if ($this->checkDiagonal($symbol)) {
+                    $this->state = GameStatue::FINISHED;
+                }
+            }
+        }
     }
-
+    private function checkDiagonal(string $symbol): bool
+    {
+        for ($i = 0; $i < self::N; $i++)
+            if ($this->board[$i][$i] != $symbol)
+                return false;
+        return true;
+    }
     public function showBoard()
     {
         echo '<table border="1">';
         foreach ($this->board as $row) {
             echo "<tr>";
             foreach ($row as $cell) {
-                echo "<td width='20' height='25' style='text-align:center'>$cell</td>";
+                echo '<td width="20" height="25" style="text-align:center;">' . $cell . '</td>';
             }
             echo "</tr>";
         }
